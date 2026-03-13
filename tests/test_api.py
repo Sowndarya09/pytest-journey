e36c976238e6ac6633ad535ac30af2c6fe67f565
Sowndarya09 <53424618+Sowndarya09@users.noreply.github.com>
2026-03-13 18:18:31 +0000

    API - GET, PUT, POST, DELETE. Also added to CI/CD pipeline

--- tests/test_api.py
+++ tests/test_api.py
@@ -8,3 +8,38 @@ def test_get_user():
     assert body['id'] == 1
     assert body['username']!= ''
     assert '@' in body['email']
+
+
+
+def test_create_user():
+    response = requests.post('https://jsonplaceholder.typicode.com/users', 
+        json={
+            'name': 'Sownd',
+            'email': 'sownd@gmail.com',
+            'username': 'sragu'
+        })
+    
+    assert response.status_code == 201
+    body = response.json()
+    assert body['name'] == 'Sownd'
+
+
+def test_update_user():
+    response = requests.put('https://jsonplaceholder.typicode.com/users/1', 
+        json={
+            'name': 'Sowndarya',
+            'email': 'sownd@gmail.com',
+            'username': 'sragu'
+        })
+    
+    assert response.status_code == 200
+    body = response.json()
+    assert body['name'] == 'Sowndarya'
+
+def test_delete_user():
+    response = requests.delete('https://jsonplaceholder.typicode.com/users/1')
+    # Note: jsonplaceholder returns 200 for DELETE
+    # In production APIs this should be 204 No Content
+    assert response.status_code == 200
+    
+
